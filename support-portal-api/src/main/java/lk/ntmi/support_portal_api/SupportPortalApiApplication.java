596780342562b924cb2560331d62fd8a8cfe51ca
Dasun Madusanka <madusankedasun@gmail.com>
2026-01-05 10:49:50 +0530

    qa

---
+++ support-portal-api/src/main/java/lk/ntmi/support_portal_api/SupportPortalApiApplication.java
@@ -0,0 +1,13 @@
+package lk.ntmi.support_portal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportPortalApiApplication.class, args);
+	}
+
+}
---
+++ support-portal-api/src/test/java/lk/ntmi/support_portal_api/SupportPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ntmi.support_portal_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportPortalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
